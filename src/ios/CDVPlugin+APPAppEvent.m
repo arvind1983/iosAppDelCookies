6fef9c24e7fa3cfc25d5ee41b0e40edb51011931
arvind1983 <41881552+arvind1983@users.noreply.github.com>
2019-12-10 16:48:07 +0800

    Update CDVPlugin+APPAppEvent.m

--- src/ios/CDVPlugin+APPAppEvent.m
+++ src/ios/CDVPlugin+APPAppEvent.m
@@ -83,11 +83,7 @@ void swizzled_pluginInitialize(id self, SEL _cmd)
 - (void)finishLaunching:(NSNotification *)notification
 {
     // Put here the code that should be on the AppDelegate.m
-    UIAlertController * alert = [UIAlertController
-                alertControllerWithTitle:@"Title"
-                                 message:@"Message"
-                          preferredStyle:UIAlertControllerStyleAlert];
-    [self presentViewController:alert animated:YES completion:nil];
+    
 
 }
 
